b34eff2fb8bd321c89391104af74ed0311bcc5be
odelolajosh <oladapoodelola@gmail.com>
2023-01-03 22:39:38 +0100

    fix: clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,4 @@
-export default function cleanSet(set, startString) {
+export default function cleanSet(set = new Set(), startString = '') {
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
