a273765726f5f7843f557876badedddfd635332e
odelolajosh <oladapoodelola@gmail.com>
2023-01-03 22:51:08 +0100

    fix: weak lint data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -8,7 +8,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0;
   weakMap.set(endpoint, count + 1);
-  if (count >= 5) {
+  if (count + 1 >= 5) {
     throw new Error('Endpoint load is high');
   }
 }
