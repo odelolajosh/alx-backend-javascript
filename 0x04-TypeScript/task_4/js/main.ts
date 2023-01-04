00b7a0f10d9dc873d242a2a7069e494424848caf
odelolajosh <oladapoodelola@gmail.com>
2023-01-05 00:36:17 +0100

    task: Namespace & Declaration merging

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,9 @@
+/// <reference types="./crud" />
+import { RowElement, RowID } from "./interface";
+import * as CRUD from "./crud";
+
+const row: RowElement = { firstName: "Guillaume", lastName: "Salva" }
+const newRowID: RowID = CRUD.insertRow(row);
+const updateRow: RowElement = { ...row, age: 23 };
+CRUD.updateRow(newRowID, updateRow);
+CRUD.deleteRow(newRowID);
---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,9 @@
+namespace Subjects {
+  export class Subject {
+    teacher: Teacher;
+
+    set Teacher(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+  }
+}
