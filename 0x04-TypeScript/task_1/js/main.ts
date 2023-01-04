98950f04e4189d5e088d6630f10fd05a09d175ce
odelolajosh <oladapoodelola@gmail.com>
2023-01-04 22:14:49 +0100

    task: writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,5 +4,46 @@ export interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [index: string]: any;
-};
+}
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+export interface StudentClassConstructor {
+  new(firstName: string, lastName: string): StudentClass;
+}
+
+export interface IStudent {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements IStudent {
+  constructor(public firstName: string, public lastName: string) {}
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
+
+const makeStudent = (factory: StudentClassConstructor, firstName: string, lastName: string) => {
+  return new factory(firstName, lastName);
+}
+
+const student = makeStudent(StudentClass, 'John', 'Doe');
+console.log(student.workOnHomework()); // Currently working
