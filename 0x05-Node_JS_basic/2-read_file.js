907d386a6e1325ff374eb0f5a5ce0871c60328b9
odelolajosh <oladapoodelola@gmail.com>
2023-02-23 12:45:20 +0100

    feat: read from file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+/**
+ * Count the number of students in a database synchronously.
+ * @param {string} path
+ * @return {Promise<number>}
+ */
+function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const content = fs.readFileSync(path, 'utf-8');
+  const rows = content.trim().split('\n');
+  const fields = rows[0].split(',');
+  const fieldPos = fields.findIndex((value) => value === 'field');
+  const fNamePos = fields.findIndex((value) => value === 'firstname');
+
+  const STUDENT_COUNT = Symbol('Student count');
+
+  const fieldMap = rows.slice(1).reduce((map, row) => {
+    const splitRow = row.split(',');
+    if (splitRow.length === 0) {
+      return map;
+    }
+    if (splitRow.length >= fieldPos + 1 && splitRow.length >= fNamePos + 1) {
+      const field = splitRow[fieldPos];
+      const firstName = splitRow[fNamePos];
+      map[field] = map[field] || [];
+      map[field].push(firstName);
+      map[STUDENT_COUNT] = (map[STUDENT_COUNT] || 0) + 1;
+    }
+
+    return map;
+  }, {});
+
+  console.log(`Number of students: ${fieldMap[STUDENT_COUNT]}`);
+  Object.entries(fieldMap).forEach(([field, names]) => {
+    console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+  });
+}
+
+module.exports = countStudents;
