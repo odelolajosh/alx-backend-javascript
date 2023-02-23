6c6f2f1b4b48da71021d9373b3b0b55d6cc1ff7c
odelolajosh <oladapoodelola@gmail.com>
2023-02-23 13:10:16 +0100

    feat: small http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((_req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
   res.write('Hello to Holberton School!');
   res.end();
 });
 
 server.listen(1245, 'localhost');
+
+module.exports = server;
