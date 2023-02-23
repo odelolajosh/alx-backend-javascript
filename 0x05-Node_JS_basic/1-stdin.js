1a3a51c1d568bb24395fec7f9ac353ed11d1436d
odelolajosh <oladapoodelola@gmail.com>
2023-02-23 11:57:29 +0100

    feat: stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+// Read from stdin and print to stdout
+
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
