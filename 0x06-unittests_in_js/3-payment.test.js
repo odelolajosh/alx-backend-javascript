61b6c94a8ffa55578881f5253a471cf4ddd4da1e
odelolajosh <oladapoodelola@gmail.com>
2023-02-24 21:48:47 +0100

    feat: some spying and async testing

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,13 +1,13 @@
 const { expect } = require("chai");
-const Sinon = require("sinon");
+const sinon = require("sinon");
 const sendPaymentRequestToApi = require("./3-payment");
 const Utils = require("./utils");
 
 describe('sendPaymentRequestToApi', () => {
   it('should call the Utils.calculateNumber with required args', () => {
-    const spy = Sinon.spy(Utils);
+    const spy = sinon.spy(Utils);
     sendPaymentRequestToApi(100, 20);
     expect(spy.calculateNumber.calledOnceWith('SUM', 100, 20)).to.be.true;
-    sinon.restore();
+    spy.calculateNumber.restore();
   });
 });
